f48b10a8967e184391ee63f10e4366b49f96f874
subin7709 <tnqls103605@naver.com>
2023-02-21 17:14:55 +0900

    MyPage
    기본 버튼(환경설정, 매출입력)

--- jeju_app/lib/view/mypage.dart
+++ jeju_app/lib/view/mypage.dart
@@ -13,12 +13,40 @@ class _MyPageState extends State<MyPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('My Page'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              'mypage',
+            Row(
+              children: [
+                const Text(
+                  "userId 님 환영합니다.",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      //--
+                    },
+                    child: const Text("환경설정",
+                        style: TextStyle(fontSize: 20, color: Colors.black)))
+              ],
+            ),
+            Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      //     Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //     builder: (context) =>  (),
+                      //   ),
+                      // );
+                    },
+                    child: const Text('매출 입력하기'))
+              ],
             )
           ],
         ),
