95cd1ae8fd2c4508b2fd9246b9750ce8c1b46457
subin7709 <tnqls103605@naver.com>
2023-02-22 19:38:00 +0900

    my page (5)

---
+++ jeju_app/lib/view/mypage_store.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:jeju_app/view/mypage_store_add.dart';
+
+class MyPage_Store extends StatefulWidget {
+  const MyPage_Store({super.key});
+
+  @override
+  State<MyPage_Store> createState() => _MyPage_StoreState();
+}
+
+class _MyPage_StoreState extends State<MyPage_Store> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('나의 가게'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const MyPage_Store_Add(),
+                  ),
+                );
+              },
+              icon: const Icon(Icons.add))
+        ],
+      ),
+    );
+  }
+}
