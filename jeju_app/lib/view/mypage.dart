2888a529422092553069fdd60f90b0f86fbaebab
subin7709 <tnqls103605@naver.com>
2023-02-22 11:32:38 +0900

    mypage

--- jeju_app/lib/view/mypage.dart
+++ jeju_app/lib/view/mypage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jeju_app/view/mypage_preferences.dart';
 
 class MyPage extends StatefulWidget {
   const MyPage({super.key});
@@ -15,38 +16,52 @@ class _MyPageState extends State<MyPage> {
         title: const Text('My Page'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Row(
-              children: [
-                const Text(
-                  "userId 님 환영합니다.",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      //--
-                    },
-                    child: const Text("환경설정",
-                        style: TextStyle(fontSize: 20, color: Colors.black)))
-              ],
-            ),
-            Row(
-              children: [
-                ElevatedButton(
-                    onPressed: () {
-                      //     Navigator.push(
-                      //   context,
-                      //   MaterialPageRoute(
-                      //     builder: (context) =>  (),
-                      //   ),
-                      // );
-                    },
-                    child: const Text('매출 입력하기'))
-              ],
-            )
-          ],
+        child: Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 20,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "userId 님 환영합니다.",
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const MyPage_Preferences(),
+                          ),
+                        );
+                      },
+                      child: const Text("환경설정",
+                          style: TextStyle(fontSize: 20, color: Colors.black))),
+                ],
+              ),
+              Row(
+                children: [
+                  ElevatedButton(
+                      onPressed: () {
+                        //     Navigator.push(
+                        //   context,
+                        //   MaterialPageRoute(
+                        //     builder: (context) =>  (),
+                        //   ),
+                        // );
+                      },
+                      child: const Text('매출 입력하기'))
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
