5ad10dd41a4c8423e49d7f7a6b0e8631f2c5640a
kosukjin <sukjinko78@gmail.com>
2023-02-24 10:31:18 +0900

    record

---
+++ jeju_app/lib/model/record_message.dart
@@ -0,0 +1,12 @@
+class Message {
+  static String sCategory = "";
+  static String mMemo = "";
+  static String mDate = "";
+  static int mProfit = 0;
+  static String mMonth = "";
+
+  static List<String> year1 = [];
+  static List<String> month1 = [];
+  static int strYear = 0; //현재 년도
+  static String strMonth = ""; //현재 월
+}
