1966ae11ae9578dd836d0a88c5ab9375b748e029
subin7709 <tnqls103605@naver.com>
2023-02-26 14:44:56 +0900

    sotre 오류 수정중
    서귀포 시면 수정 삭게 페이지 못들어감, set문제 아직 있음

--- jeju_spring/src/main/java/com/jeju/base/dao/UserStoreDao.java
+++ jeju_spring/src/main/java/com/jeju/base/dao/UserStoreDao.java
@@ -9,7 +9,7 @@ public interface UserStoreDao {
 	public List<UserStoreDto> UserStoreSelect(String uId) throws Exception;
 	public void UserStoreAdd(String sName, String sCategory,String sDong,String sTel,String sAddress,String user_uId,String user_uIdType);
 	public void UserStoreDelete(int sId);
-	public void UserStoreUpdate(String sName, String sCategory,String sDong,String sTel,String sAddress, int sId);
+	public void UserStoreUpdate(String sName, String sCategory,String sDong,String sTel, String sAddress, int sId);
 	
 	public List<UserDateDto> UserDateSelect(String uId) throws Exception;
 }
